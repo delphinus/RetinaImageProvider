585ec2a93149cf42cea7c66477a882565278bb0a
delphinus35 <delphinus@remora.cx>
2012-08-31 09:29:16 +0900

    deployment config

--- config/deployment.pl
+++ config/deployment.pl
@@ -3,16 +3,21 @@ my $basedir = file(__FILE__)->parent->parent;
 my $dotcloud = dir('/home/dotcloud');
 my $dbpath = -d $dotcloud ? $dotcloud->file('deployment.db')
     : $basedir->file('db', 'deployment.db');
-my $imgpath = dir('/Library/WebServer/Documents/blog/assets_c');
--d $imgpath or die;
+my $imgpath = $basedir->subdir('img');
+-d $imgpath or $imgpath->mkpath;
 +{
     DBI => [
         "dbi:SQLite:dbname=$dbpath",
         '',
         '',
         +{
-            sqlite_unicode => 1,
+            #sqlite_unicode => 1,
         }
     ],
     IMGPATH => $imgpath->stringify,
+    validator => +{
+        messages => $basedir->file('config', 'messages.yml')->stringify,
+        profiles => $basedir->file('config', 'profiles.yml')->stringify,
+        message_decode_from => 'UTF-8',
+    },
 };
