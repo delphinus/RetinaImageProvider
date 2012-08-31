b22d931fe7f8eb95913bfd73ee2eb049b5d21817
delphinus35 <delphinus@remora.cx>
2012-08-31 09:00:09 +0900

    add sample js

---
+++ static/js/imageprovider.js
@@ -0,0 +1,44 @@
+;'use strict';
+
+(function($){$(document).ready(function(){
+
+
+var Page = function(){};
+Page.prototype = {
+    opt: {
+        $entry: $('.entry-content')
+        ,maxWidth: $entry.width()
+        ,url: 'http://rip.remora.cx/'
+        ,isRetina: window.devicePixelRatio === 2
+    }
+    ,init: function() {
+        var self = this;
+
+        $('img', self.opt.$entry).each(function() {
+            var $this = $(this)
+                ,imgpath = $this.attr('src')
+                    .replace('http://', '')
+                    .replace(location.host, '')
+                ,targetWidth = $this.parent().width()
+                ,targetHeight = $this.parent().height()
+                ,reqWidth = targetWidth * (self.opt.isRetina ? 2 : 1)
+                ,reqHeight = targetHeight * (self.opt.isRetina ? 2 : 1)
+            ;
+
+            if ($this.width() <= self.opt.maxWidth) {
+                return;
+            }
+
+            $this.prop({
+                src: self.opt.url + '?w=' + reqWidth + '&h=' + reqHeight
+                ,height: targetHeight
+                ,width: targetWidth
+            });
+        })
+    }
+};
+
+window.rip = (new Page).init();
+
+
+});})(jQuery);
